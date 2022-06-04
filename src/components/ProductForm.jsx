b3a2824e7944fa05447b6d1eef8d4514692b7a14
Mehmet Baran Nakipoğlu <mehmetbarannakipoglu@outlook.com>
2022-06-05 02:43:03 +0300

    formData method for addProduct

--- src/components/ProductForm.jsx
+++ src/components/ProductForm.jsx
@@ -4,9 +4,74 @@ import styled from "styled-components"
 import { mobile } from "../responsive"
 import { Button, makeStyles } from '@material-ui/core';
 import { useNavigate } from "react-router-dom";
-import {turkeyData} from "../cityDistrict"
-import ProductPhotoUpload from "./ProductPhotoUpload";
+import { turkeyData } from "../cityDistrict"
+import CameraAltIcon from '@mui/icons-material/CameraAlt';
+import ProductPlaceHolder from '../pictures/product.png'
+import axios from 'axios';
 //const jwt = require('jsonwebtoken');
+
+//-----
+
+const ContainerPhoto = styled.div`
+    width: 100%;
+    border-radius: 10px;
+    background-color:  rgb(53, 133, 139, 0.2); 
+	padding: 20px;
+`
+const UploadContainer = styled.div`
+  display:flex;
+  align-items:center;
+  justify-content: center;
+  padding: 20px;
+  margin-bottom: 30px;
+
+`
+
+const ImgHolder = styled.div`
+    margin: auto;
+    width: 300px;
+    height: 250px;
+	  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    margin: 0 30px;
+
+`
+const ImageUpload = styled.label`
+    color: white;
+    border-radius: 20px;
+    background-color: #072227;
+    text-align: center;
+    cursor: pointer;
+  	display: flex;
+    flex-direction: column;
+	  justify-content: center;
+  	align-items: center;
+    padding: 10px;
+`
+const UploadLabel = styled.div`
+    display: flex;
+    justify-content: center;
+    
+`
+const Img = styled.img`
+	width: 300px;
+    height: 250px;
+    object-fit: cover;
+`
+
+const InputImg = styled.input.attrs({
+    type: 'file',
+})`
+
+	display: none;
+  `
+
+const WrapperPhoto = styled.div``
+
+
+
+//------
+
+
 
 const useStyles = makeStyles((theme) => ({
     button: {
@@ -29,6 +94,7 @@ const useStyles = makeStyles((theme) => ({
     }
 
 }));
+
 
 const Container = styled.div`
     background-color: rgba(0,0,0,0.1);
@@ -74,7 +140,7 @@ const Error = styled.span`
     color: red;
     justify-content: "center";
 `
-const InputTitle =styled.label`
+const InputTitle = styled.label`
     font-size: 20px;
     font-weight: 500;
     color: #35858B;
@@ -127,55 +193,101 @@ const ProductForm = () => {
     const [errorMessage, setErrorMessage] = useState('');
     const CategoryArr = ["Furniture", "Men", "Women", "Technology", "Home", "Books", "Baby-Kid", "Fun", "Travel", "School", "Elderly", "Other"];
 
+    const [productPicture, setProductPicture] = useState('');
+    const [currentImg, setCurrentImg] = useState(ProductPlaceHolder);
+
+    const imageHandler = (e) => {
+
+        const reader = new FileReader();
+
+        reader.onload = () => {
+            if (reader.readyState === 2) {
+                setCurrentImg(reader.result)
+                setProductPicture(e.target.files[0])
+            }
+        }
+        if (e.target.files[0]) {
+            reader.readAsDataURL(e.target.files[0]);
+        }
+    };
+
 
     async function addProduct(e) {
         e.preventDefault()
-        
+
         if (title < 3) {
             alert("Title should be at least 3 characters long!")
             setTitle('')
         }
 
-        else{
-        const response = await fetch('http://localhost:5000/user/addProduct', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'x-access-token': localStorage.getItem('token'),
-            },
-            body: JSON.stringify({
-                title,
-                description,
-                category,
-                shipping,
-                city,
-                district,
-            }),
-        })
+        else {
 
-        const data = await response.json()
-        if (data.status === 'ok') {
-            resetForm();
-            navigate('/Profile');
+
+            const formData = new FormData();
+
+            formData.append('title', title);
+            formData.append('description', description);
+            formData.append('category', category);
+            formData.append('shipping', shipping);
+            formData.append('city', city);
+            formData.append('district', district);
+            formData.append('productPicture', productPicture);
+            formData.append('token', localStorage.getItem('token'))
+            console.log("frontend token: " + localStorage.getItem('token'))
+            console.log('frontend productpic: ' + productPicture)
+
+            /* const response = await fetch('http://localhost:5000/user/addProduct', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'x-access-token': localStorage.getItem('token'),
+                },
+                body: JSON.stringify({
+                    title,
+                    description,
+                    category,
+                    shipping,
+                    city,
+                    district,
+                }),
+            }) */
+
+            /* const response = await fetch('http://localhost:5000/user/addProduct', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'x-access-token': localStorage.getItem('token'),
+                },
+                body: formData
+            }) */
+
+
+            const response = await axios.post('http://localhost:5000/user/addProduct', formData)
+
+            //const data = await response.json()
+            if (response.data.status === 'ok') {
+                resetForm();
+                navigate('/Profile');
+            }
+            if (response.data.status === 'error') {
+                setErrorMessage("User session ended!");
+            }
         }
-        if (data.status === 'errorToken') {
-            setErrorMessage("User session ended!");
-        }
-    }}
+    }
 
     const resetForm = () => {
         setTitle('')
         setDescription('')
-        setCategory('') 
-        setCity('') 
+        setCategory('')
+        setCity('')
         setDistrict('')
         setDistrictArr([])
         setShipping('')
     }
 
-    const changeCity =(e)=>{
-        setCity(e.target.value) ;
-        setDistrictArr( (turkeyData.find( ( selectedCity ) => selectedCity.il_adi === e.target.value)).ilceler);
+    const changeCity = (e) => {
+        setCity(e.target.value);
+        setDistrictArr((turkeyData.find((selectedCity) => selectedCity.il_adi === e.target.value)).ilceler);
     }
 
     return (
@@ -184,54 +296,73 @@ const ProductForm = () => {
                 <Wrapper>
                     <Title> ADD PRODUCT </Title>
 
-                    <Form onSubmit={addProduct}>
-                   
-                        <ProductPhotoUpload />
+                    <Form onSubmit={addProduct} encType='multipart/form-data'>
+
+                        <ContainerPhoto>
+                            <UploadContainer>
+                                <div>
+                                    <ImgHolder>
+                                        <Img src={currentImg} alt="" id="img" className="img" />
+                                    </ImgHolder>
+                                    <InputImg type="file" accept="image/*" name="image-upload" id="input" onChange={imageHandler} />
+
+                                </div>
+
+                                <Wrapper>
+                                    <UploadLabel >
+                                        <ImageUpload htmlFor="input" >
+                                            <h4>Choose Your Product Photo</h4>	<CameraAltIcon sx={{ margin: "5px", fontSize: 30 }} /></ImageUpload>
+                                    </UploadLabel>
+
+                                </Wrapper>
+                            </UploadContainer>
+
+                        </ContainerPhoto>
 
                         <InputBox>
-                        <InputTitle>Title</InputTitle>
-                        <Input placeholder="Title" required type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
-                        </InputBox>
-
-                        <InputBox>
-                        <InputTitle>Description</InputTitle>
-                        <TextArea placeholder="Description" type="textarea" required value={description} onChange={(e) => setDescription(e.target.value)} />
-                        </InputBox>
-                             
-                        <InputBox>
-                        <InputTitle for="category">Category</InputTitle>
-                        <Select name="categories" id="category" placeholder='Category' value={category} onChange={(e) => setCategory(e.target.value)}  >
-                        {CategoryArr.map((item) => (
-                             <option value={item}>{item}</option>
-                            ))}
-                        </Select>
-                        </InputBox>
-                  
-                        <InputBox>
-                        <InputTitle for="shipping">Shipping Options</InputTitle>
-                        <Select name="shipping" id="ship" placeholder='Shipping Options' value={shipping} onChange={(e) => setShipping(e.target.value)}  >
-                             <option value="By-courier">By Cargo</option>
-                             <option value="By-hand">Pick up</option>
-                             <option value="Both">Both Cargo and Pick-up</option>
-                        </Select>
+                            <InputTitle>Title</InputTitle>
+                            <Input placeholder="Title" required type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
                         </InputBox>
 
                         <InputBox>
-                        <InputTitle for="city">City</InputTitle>
-                        <Select name="cities" id="city" placeholder='City' value={city} onChange={changeCity}  >
-                        {turkeyData.map((item) => (
-                             <option value={item.il_adi}>{item.il_adi}</option>
-                            ))}
-                        </Select>
+                            <InputTitle>Description</InputTitle>
+                            <TextArea placeholder="Description" type="textarea" required value={description} onChange={(e) => setDescription(e.target.value)} />
                         </InputBox>
 
                         <InputBox>
-                        <InputTitle for="district">District</InputTitle>
-                        <Select name="district" id="dist" placeholder='District' value={district} onChange={(e) => setDistrict(e.target.value)}  >
-                            {districtArr.map((item) => (
-                                <option value={item.ilce_adi}>{item.ilce_adi}</option>
+                            <InputTitle for="category">Category</InputTitle>
+                            <Select name="categories" id="category" placeholder='Category' value={category} onChange={(e) => setCategory(e.target.value)}  >
+                                {CategoryArr.map((item) => (
+                                    <option value={item}>{item}</option>
                                 ))}
-                        </Select>
+                            </Select>
+                        </InputBox>
+
+                        <InputBox>
+                            <InputTitle for="shipping">Shipping Options</InputTitle>
+                            <Select name="shipping" id="ship" placeholder='Shipping Options' value={shipping} onChange={(e) => setShipping(e.target.value)}  >
+                                <option value="By-courier">By Cargo</option>
+                                <option value="By-hand">Pick up</option>
+                                <option value="Both">Both Cargo and Pick-up</option>
+                            </Select>
+                        </InputBox>
+
+                        <InputBox>
+                            <InputTitle for="city">City</InputTitle>
+                            <Select name="cities" id="city" placeholder='City' value={city} onChange={changeCity}  >
+                                {turkeyData.map((item) => (
+                                    <option value={item.il_adi}>{item.il_adi}</option>
+                                ))}
+                            </Select>
+                        </InputBox>
+
+                        <InputBox>
+                            <InputTitle for="district">District</InputTitle>
+                            <Select name="district" id="dist" placeholder='District' value={district} onChange={(e) => setDistrict(e.target.value)}  >
+                                {districtArr.map((item) => (
+                                    <option value={item.ilce_adi}>{item.ilce_adi}</option>
+                                ))}
+                            </Select>
                         </InputBox>
 
                         <Error> {errorMessage} </Error>
--- src/components/ProductPhotoUpload.jsx
+++ src/components/ProductPhotoUpload.jsx
@@ -1,18 +1,15 @@
-import styled from "styled-components"
-import React, { useState, useEffect} from 'react';
-import CameraAltIcon from '@mui/icons-material/CameraAlt';
-import ProductPlaceHolder from '../pictures/product.png'
-import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
-import HighlightOffIcon from '@mui/icons-material/HighlightOff';
-import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 
-const Container = styled.div`
+
+
+
+
+const ContainerPhoto = styled.div`
     width: 100%;
     border-radius: 10px;
     background-color:  rgb(53, 133, 139, 0.2); 
 	padding: 20px;
 `
-const UploadContainer= styled.div`
+const UploadContainer = styled.div`
   display:flex;
   align-items:center;
   justify-content: center;
@@ -46,109 +43,65 @@ const UploadLabel = styled.div`
     justify-content: center;
     
 `
-const Img =styled.img`
+const Img = styled.img`
 	width: 300px;
     height: 250px;
     object-fit: cover;
 `
-const UploadButtons =styled.div`
-  display:flex;
-  align-items: center;
-  justify-content: center;
-  margin: 30px 0 10px 0;
-`
 
-const Input = styled.input.attrs({ 
-	type: 'file',
-  })`
+
+const Input = styled.input.attrs({
+  type: 'file',
+})`
 
 	display: none;
   `
-const ImageAlbum = styled.div`
-  background-color:  rgb(0,0,0, 0.2); 
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: flex-start;
-`
-const Wrapper =styled.div``
+
+const WrapperPhoto = styled.div``
 
 
-const UploadedImg = styled.img`
-  width: 200px;
-  height: 200px;
-  object-fit: cover;
-  margin: 15px;
-`
-const PhotoWrapper = styled.div``
-  
-  
-  const ProfilePhotoUpload = () => {
-    const [imageArr, setImageArr] = useState([]);
-    const [clicked, setClicked] = useState(false);
-    const [currentImg, setCurrentImg] = useState(ProductPlaceHolder);
-    
+
+const ProfilePhotoUpload = () => {
+  const [productPicture, setProductPicture] = useState('');
+  const [currentImg, setCurrentImg] = useState(ProductPlaceHolder);
+
   const imageHandler = (e) => {
-    setClicked(true);
+
     const reader = new FileReader();
-    reader.onload = () =>{
-      if(reader.readyState === 2){
+
+    reader.onload = () => {
+      if (reader.readyState === 2) {
         setCurrentImg(reader.result)
+        setProductPicture(e.target.files[0])
       }
     }
-    reader.readAsDataURL(e.target.files[0])
+    if (e.target.files[0]) {
+      reader.readAsDataURL(e.target.files[0]);
+    }
   };
 
-
-  const approvePhoto=(e) =>{
-      console.log("approved");
-      setCurrentImg(ProductPlaceHolder);
-      setImageArr(oldArr => [...oldArr, currentImg]);
-      setClicked(false);
-  }
-
-  const cancelPhoto=(e) =>{
-    setClicked(false);
-    setCurrentImg(ProductPlaceHolder);
-  }
-
-		return (
-			<Container>
+  return (
+    <ContainerPhoto>
       <UploadContainer>
-      <div>
-        <ImgHolder>
+        <div>
+          <ImgHolder>
             <Img src={currentImg} alt="" id="img" className="img" />
           </ImgHolder>
           <Input type="file" accept="image/*" name="image-upload" id="input" onChange={imageHandler} />
 
-        {clicked ? 
-        <UploadButtons >
-        <CheckCircleOutlineIcon sx={{ fontSize: 60, marginRight: "15px", }} onClick={approvePhoto}/>
-        <HighlightOffIcon sx={{ fontSize: 60 }} onClick={cancelPhoto}/>
-        </UploadButtons>
-        : ""}
         </div>
 
-          
+        <WrapperPhoto>
+          <UploadLabel >
+            <ImageUpload htmlFor="input" >
+              <h4>Choose Your Product Photo</h4>	<CameraAltIcon sx={{ margin: "5px", fontSize: 30 }} /></ImageUpload>
+          </UploadLabel>
 
-        <Wrapper>
-      	<UploadLabel >
-				<ImageUpload htmlFor="input" >
-				 <h4>Choose Your Product Photo</h4>	<CameraAltIcon sx={{margin: "5px", fontSize: 30}} /></ImageUpload>
-				</UploadLabel>
+        </WrapperPhoto>
+      </UploadContainer>
 
-        </Wrapper>
-        </UploadContainer>
-
-        <ImageAlbum>
-          {imageArr.map((item) => (
-            <PhotoWrapper>
-            <UploadedImg src={item}/>
-            </PhotoWrapper>
-            ))}
-        </ImageAlbum>
-         
-			</Container>
-		);
-	}
+    </ContainerPhoto>
+  );
+}
 
 export default ProfilePhotoUpload;
